9a32cb9c1a3f16ddb9843f91df7952e99c0e872e
amin roosta <amin.roosta@outlook.com>
2018-05-13 18:01:39 +0800

    add PlusIcon

--- App.js
+++ App.js
@@ -1,5 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ART, View } from 'react-native';
+
+const {
+  Surface,
+  Shape,
+  Group,
+  Path,
+  ClippingRectangle,
+  LinearGradient,
+  RadialGradient,
+  Pattern,
+  Transform
+} = ART;
+
+class PlusIcon extends React.Component {
+    render() {
+        const path = [
+		 [ 'M', 8, 12 ], [ 'L', 8, 20 ], [ 'L', 12, 20 ], [ 'L', 12, 12 ], [ 'L', 20, 12 ], [ 'L', 20, 8 ],
+		 [ 'L', 12, 8 ], [ 'L', 12, 0 ], [ 'L', 8, 0 ], [ 'L', 8, 8 ], [ 'L', 0, 8 ], [ 'L', 0, 12 ], [ 'L', 8, 12 ]
+		];
+
+		const {width = 10, height = 10} = this.props;
+		const viewBox = 20;
+		const rx = width/viewBox, ry = height/viewBox;
+
+		const d = path.map(([t, x, y]) => t + (x*rx) + ',' + (y*ry)).join(' ');
+        return (
+            <Surface width={width} height={height}>
+				<Shape
+                    fill="#00AFF8" 
+					d={d + ' Z'}
+				></Shape>
+            </Surface>
+        );
+    }
+}
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +43,8 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+		<PlusIcon
+			width={50} height={50} />
       </View>
     );
   }
---
+++ scripts/parse-path.js
@@ -0,0 +1,4 @@
+const parse = require('parse-svg-path');
+
+console.log(parse("M8,12 L8,20 L12,20 L12,12 L20,12 L20,8 L12,8 L12,0 L8,0 L8,8 L0,8 L0,12 L8,12 Z"));
+
